ab2db399e2ad3cf2473059cb33c6956de0605ccb
Matthew Draper <draper_matthewjohn@yahoo.com>
2024-01-01 10:50:01 -0800

    remove some bulky stuff, and some small stuff I do not actualy care about

--- ctok_raw/open-watcom-v2-2023-12-01-Build/bld/wprof/h/memutil.h
+++
@@ -1,8 +0,0 @@
-
-extern void     *ProfAlloc( size_t size );
-extern void     ProfFree( void *ptr );
-extern void     *ProfRealloc( void *p, size_t new_size );
-extern void     *ProfCAlloc( size_t size );
-extern void     WPMemOpen( void );
-extern void     WPMemClose( void );
-extern void     WPMemPrtUsage( void );
